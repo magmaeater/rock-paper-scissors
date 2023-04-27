6532a6b68889e430b8afffcc863ab07f7ded6397
magma <tanayachyute@gmail.com>
2023-04-27 18:45:09 +0530

    write js for getPlayerChoice function

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,10 +1,10 @@
 function getPlayerChoice()
 {
-        let choice = prompt("Enter rock, paper or scissors \n");
+        let choice = prompt("Enter rock, paper or scissors");
         
         if(choice.toLowerCase() === 'rock' || choice.toLowerCase() === 'paper'|| choice.toLowerCase() === 'scissors')
         {
-            return choice;
+            return choice.toLowerCase();
         }
         else
         {
@@ -25,9 +25,31 @@ function getComputerChoice()
 }
 
 function playRound(playerSelection, computerSelection)
-{
-    console.log(playerSelection);
-    console.log(computerSelection);
+{   
+    if(playerSelection === false)
+    {
+        console.log("invalid input");
+        return;
+    }
+    
+    let win = playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper";
+    let draw = playerSelection === computerSelection;
+    
+    if(draw === true)
+    {
+        console.log(`draw!!\ncomputer - ${computerSelection}\nplayer - ${playerSelection.toLowerCase()}`);
+    }
+    else if(win === false)
+    {
+        console.log(`you lose!! you picked ${playerSelection.toLowerCase()} and the computer picked ${computerSelection}`);
+    }
+    else if(win === true)
+    {
+        console.log(`you win!! you picked ${playerSelection.toLowerCase()} and the computer picked ${computerSelection}`);
+    }
+    
+    
+    
 }
 
 playRound(getPlayerChoice(), getComputerChoice());
