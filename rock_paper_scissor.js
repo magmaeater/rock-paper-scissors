80f85135f640efeea4565d826df590adad3bd030
magma <tanayachyute@gmail.com>
2023-04-27 17:47:17 +0530

    write required functions for program

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,1 +1,33 @@
-console.log("hello world!");
+function getPlayerChoice()
+{
+        let choice = prompt("Enter rock, paper or scissors \n");
+        
+        if(choice.toLowerCase() === 'rock' || choice.toLowerCase() === 'paper'|| choice.toLowerCase() === 'scissors')
+        {
+            return choice;
+        }
+        else
+        {
+            return false;
+        } 
+}
+
+function getComputerChoice()
+{
+    let choices = [
+        "rock",
+        "paper",
+        "scissors"
+    ];
+
+    let choice = Math.floor(Math.random() * choices.length);
+    return choices[choice];
+}
+
+function playRound(playerSelection, computerSelection)
+{
+    console.log(playerSelection);
+    console.log(computerSelection);
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
