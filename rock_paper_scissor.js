13f79dcf491eada87b716ce64b9e885b9fd0bac3
magma <tanayachyute@gmail.com>
2023-04-27 19:30:06 +0530

    write js for main game loop

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -24,7 +24,7 @@ function getComputerChoice()
     return choices[choice];
 }
 
-function playRound(playerSelection, computerSelection)
+function playRound(playerSelection, computerSelection, pCount, cCount)
 {   
     if(playerSelection === false)
     {
@@ -38,18 +38,66 @@ function playRound(playerSelection, computerSelection)
     if(draw === true)
     {
         console.log(`draw!!\ncomputer - ${computerSelection}\nplayer - ${playerSelection.toLowerCase()}`);
+        return "draw";
     }
     else if(win === false)
     {
         console.log(`you lose!! you picked ${playerSelection.toLowerCase()} and the computer picked ${computerSelection}`);
+        return "loss";
     }
     else if(win === true)
     {
         console.log(`you win!! you picked ${playerSelection.toLowerCase()} and the computer picked ${computerSelection}`);
-    }
-    
-    
-    
+        return "win";
+    }     
 }
 
-playRound(getPlayerChoice(), getComputerChoice());
+function game()
+{
+    let pCount = 0;
+    let cCount = 0;
+    for(let i = 0; i < 5; i++)
+    {
+        console.log(`round ${i+1}:\nplayer score: ${pCount}\n computer score: ${cCount}`);
+        let pChoice = getPlayerChoice();
+        let cChoice = getComputerChoice();
+        if(pChoice === false)
+        {
+            console.log("wrong input! try again");
+            i--;
+            continue;
+        }
+
+        let result = playRound(pChoice, cChoice);
+        if(result === "win")
+        {
+            pCount++;
+        }
+        else if(result === "loss")
+        {
+            cCount++;
+        }
+        else{
+            pCount++;
+            cCount++;
+        }
+    }
+    if(pCount > cCount)
+    {
+        console.log("you win the overall match, well done!");
+        console.log(`final score - \nplayer - ${pCount}\ncomputer - ${cCount}`);
+    }
+    else if (pCount < cCount)
+    {
+        console.log("you losr the overall match, try again!");
+        console.log(`final score - \nplayer - ${pCount}\ncomputer - ${cCount}`);
+    }
+    else
+    {
+        console.log(`it was a draw for the overall match!`);
+        console.log(`final score - \nplayer - ${pCount}\ncomputer - ${cCount}`);
+    }
+
+}
+
+game();
